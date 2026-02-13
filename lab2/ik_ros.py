e4d3208384926f737427364c1845ab914a1b60d8
Hanze-L <hanzel@andrew.cmu.edu>
2026-02-13 14:31:23 -0500

    lab2 part2

--- lab2/ik_ros.py
+++ lab2/ik_ros.py
@@ -9,7 +9,7 @@ import rclpy
 
 # NOTE before running: `python3 -m pip install --upgrade ikpy graphviz urchin networkx`
 
-target_point = [-0.043, -0.441, 0.654]
+target_point = [0.53, -0.441, 0.2]
 target_orientation = ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi/2) # [roll, pitch, yaw]
 
 
@@ -47,8 +47,7 @@ class IKNode:
         names_of_links_to_remove = ['link_right_wheel', 'link_left_wheel', 'caster_link', 'link_head', 'link_head_pan', 'link_head_tilt', 'link_aruco_right_base', 'link_aruco_left_base', 'link_aruco_shoulder', 'link_aruco_top_wrist', 'link_aruco_inner_wrist', 'camera_bottom_screw_frame', 'camera_link', 'camera_depth_frame', 'camera_depth_optical_frame', 'camera_infra1_frame', 'camera_infra1_optical_frame', 'camera_infra2_frame', 'camera_infra2_optical_frame', 'camera_color_frame', 'camera_color_optical_frame', 'camera_accel_frame', 'camera_accel_optical_frame', 'camera_gyro_frame', 'camera_gyro_optical_frame', 'gripper_camera_bottom_screw_frame', 'gripper_camera_link', 'gripper_camera_depth_frame', 'gripper_camera_depth_optical_frame', 'gripper_camera_infra1_frame', 'gripper_camera_infra1_optical_frame', 'gripper_camera_infra2_frame', 'gripper_camera_infra2_optical_frame', 'gripper_camera_color_frame', 'gripper_camera_color_optical_frame', 'laser', 'base_imu', 'respeaker_base', 'link_wrist_quick_connect', 'link_gripper_finger_right', 'link_gripper_fingertip_right', 'link_aruco_fingertip_right', 'link_gripper_finger_left', 'link_gripper_fingertip_left', 'link_aruco_fingertip_left', 'link_aruco_d405', 'link_head_nav_cam']
         # links_kept = ['base_link', 'link_mast', 'link_lift', 'link_arm_l4', 'link_arm_l3', 'link_arm_l2', 'link_arm_l1', 'link_arm_l0', 'link_wrist_yaw', 'link_wrist_yaw_bottom', 'link_wrist_pitch', 'link_wrist_roll', 'link_gripper_s3_body', 'link_grasp_center']
         links_to_remove = [l for l in modified_urdf._links if l.name in names_of_links_to_remove]
-        for lr in links_to_remove:
-            modified_urdf._links.remove(lr)
+        modified_urdf._links.remove(lr)
         names_of_joints_to_remove = ['joint_right_wheel', 'joint_left_wheel', 'caster_joint', 'joint_head', 'joint_head_pan', 'joint_head_tilt', 'joint_aruco_right_base', 'joint_aruco_left_base', 'joint_aruco_shoulder', 'joint_aruco_top_wrist', 'joint_aruco_inner_wrist', 'camera_joint', 'camera_link_joint', 'camera_depth_joint', 'camera_depth_optical_joint', 'camera_infra1_joint', 'camera_infra1_optical_joint', 'camera_infra2_joint', 'camera_infra2_optical_joint', 'camera_color_joint', 'camera_color_optical_joint', 'camera_accel_joint', 'camera_accel_optical_joint', 'camera_gyro_joint', 'camera_gyro_optical_joint', 'gripper_camera_joint', 'gripper_camera_link_joint', 'gripper_camera_depth_joint', 'gripper_camera_depth_optical_joint', 'gripper_camera_infra1_joint', 'gripper_camera_infra1_optical_joint', 'gripper_camera_infra2_joint', 'gripper_camera_infra2_optical_joint', 'gripper_camera_color_joint', 'gripper_camera_color_optical_joint', 'joint_laser', 'joint_base_imu', 'joint_respeaker', 'joint_wrist_quick_connect', 'joint_gripper_finger_right', 'joint_gripper_fingertip_right', 'joint_aruco_fingertip_right', 'joint_gripper_finger_left', 'joint_gripper_fingertip_left', 'joint_aruco_fingertip_left', 'joint_aruco_d405', 'joint_head_nav_cam'] 
         # joints_kept = ['joint_mast', 'joint_lift', 'joint_arm_l4', 'joint_arm_l3', 'joint_arm_l2', 'joint_arm_l1', 'joint_arm_l0', 'joint_wrist_yaw', 'joint_wrist_yaw_bottom', 'joint_wrist_pitch', 'joint_wrist_roll', 'joint_gripper_s3_body', 'joint_grasp_center']
         joints_to_remove = [l for l in modified_urdf._joints if l.name in names_of_joints_to_remove]
@@ -92,7 +91,26 @@ class IKNode:
         new_urdf_path = "/tmp/iktutorial/stretch.urdf"
         modified_urdf.save(new_urdf_path)
 
-        chain = ikpy.chain.Chain.from_urdf_file(new_urdf_path)
+        active_links_mask = [
+            False,  # 0:  Base link (fixed)
+            True,   # 1:  joint_base_rotation (revolute)
+            True,   # 2:  joint_base_translation (prismatic)
+            False,  # 3:  joint_mast (fixed)
+            True,   # 4:  joint_lift (prismatic)
+            False,  # 5:  joint_arm_l4 (fixed)
+            True,   # 6:  joint_arm_l3 (prismatic)
+            True,   # 7:  joint_arm_l2 (prismatic)
+            True,   # 8:  joint_arm_l1 (prismatic)
+            True,   # 9:  joint_arm_l0 (prismatic)
+            True,   # 10: joint_wrist_yaw (revolute)
+            False,  # 11: joint_wrist_yaw_bottom (fixed)
+            True,   # 12: joint_wrist_pitch (revolute)
+            True,   # 13: joint_wrist_roll (revolute)
+            False,  # 14: joint_gripper_s3_body (fixed)
+            False,  # 15: joint_grasp_center (fixed)
+        ]
+
+        chain = ikpy.chain.Chain.from_urdf_file(new_urdf_path, active_links_mask=active_links_mask)
 
         for link in chain.links:
             print(f"* Link Name: {link.name}, Type: {link.joint_type}")
@@ -123,15 +141,16 @@ class IKNode:
         q_yaw = q[10]
         q_pitch = q[12]
         q_roll = q[13]
-        self.node.move_to_pose({'rotate_mobile_base': q_rotation})
-        self.node.move_to_pose({'translate_mobile_base': q_translation})
         self.node.move_to_pose({
         'joint_lift': q_lift,
         'joint_arm': q_arm,
         'joint_wrist_yaw': q_yaw,
         'joint_wrist_pitch': q_pitch,
         'joint_wrist_roll': q_roll
-    })
+        })
+        self.node.move_to_pose({'rotate_mobile_base': q_rotation})
+        self.node.move_to_pose({'translate_mobile_base': q_translation})
+
 
     def move_to_grasp_goal(self, target_point, target_orientation):
         q_init = self.get_current_configuration()
@@ -152,17 +171,15 @@ class IKNode:
 
 
 def main():
-    rclpy.init()
-    
-    try:
-        ik_node = IKNode()
-        ik_node.move_to_grasp_goal(target_point, target_orientation)
-        print("Current grasp pose:")
-        print(ik_node.get_current_grasp_pose())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        rclpy.shutdown()
+   
+    ik_node = IKNode()
+    # ik_node.node.stow_the_robot()
+    ik_node.move_to_grasp_goal(target_point, target_orientation)
+    print("Current grasp pose:")
+    print(ik_node.get_current_grasp_pose())
+
+    ik_node.node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
---
+++ lab2/moveit2.py
@@ -0,0 +1,93 @@
+import rclpy, time
+import numpy as np
+from geometry_msgs.msg import Pose, PoseStamped
+from moveit.core.robot_state import RobotState
+from shape_msgs.msg import SolidPrimitive
+from control_msgs.action import FollowJointTrajectory
+from hello_helpers.hello_misc import HelloNode
+import moveit2_utils
+
+# Make sure to run `ros2 launch stretch_core stretch_driver.launch.py`
+
+class MoveMe(HelloNode):
+    def __init__(self):
+        HelloNode.__init__(self)
+        self.main('move_me', 'move_me', wait_for_first_pointcloud=False)
+        self.stow_the_robot()
+
+        planning_group = 'mobile_base_arm'
+        moveit, moveit_plan, planning_params = moveit2_utils.setup_moveit(planning_group)
+        
+        for i in range(4):
+            print(f'--- Planning Step {i} ---')
+            goal_state = RobotState(moveit.get_robot_model())
+
+        # Keep arm/wrist at current positions
+            current_joints = [
+                self.get_joint_pos('joint_lift'), 
+                self.get_joint_pos('joint_arm_l3'), self.get_joint_pos('joint_arm_l2'), 
+                self.get_joint_pos('joint_arm_l1'), self.get_joint_pos('joint_arm_l0'), 
+                self.get_joint_pos('joint_wrist_yaw'), self.get_joint_pos('joint_wrist_pitch'), 
+                self.get_joint_pos('joint_wrist_roll')
+            ]
+
+            if i == 0:
+                # Position 1: (-20cm, 20cm)
+                base_goal = [-0.2, 0.2, 0.0]
+            elif i == 1:
+                # Position 2: (40cm, 20cm)
+                base_goal = [0.4, 0.2, 0.0]
+            elif i == 2:
+                # Position 3: (20cm, -20cm)
+                base_goal = [0.2, -0.2, 0.0]
+            else:
+                # Position 4: Return to origin
+                base_goal = [0.0, 0.0, 0.0]
+
+            goal_state.set_joint_group_positions(planning_group, base_goal + current_joints)
+            
+            moveit_plan.set_start_state_to_current_state()
+            moveit_plan.set_goal_state(robot_state=goal_state)
+            
+            plan = moveit_plan.plan(parameters=planning_params)
+            self.execute_plan(plan)
+
+    def execute_plan(self, plan):
+        # NOTE: You don't need to edit this function
+        processor = moveit2_utils.TrajectoryProcessor()
+        segments = processor.process_trajectory(plan, self.joint_state)
+
+        for i, goal_traj in enumerate(segments):
+            # print(goal_traj)
+            # time.sleep(2.0)
+            self.get_logger().info(f"Executing segment {i+1}/{len(segments)} (Mode: {self._detect_mode(goal_traj)})")
+            
+            goal = FollowJointTrajectory.Goal()
+            goal.trajectory = goal_traj
+            
+            future = self.trajectory_client.send_goal_async(goal)
+            rclpy.spin_until_future_complete(self, future)
+            goal_handle = future.result()
+            
+            if not goal_handle.accepted:
+                self.get_logger().error(f"Segment {i+1} rejected!")
+                break
+            
+            res_future = goal_handle.get_result_async()
+            rclpy.spin_until_future_complete(self, res_future)
+            res = res_future.result()
+            
+            if res.result.error_code != res.result.SUCCESSFUL:
+                self.get_logger().error(f"Segment {i+1} failed! Code: {res.result.error_code}")
+                break
+
+    def get_joint_pos(self, joint_name):
+        return self.joint_state.position[self.joint_state.name.index(joint_name)]
+        
+    def _detect_mode(self, traj):
+        if 'translate_mobile_base' in traj.joint_names: return 'TRANSLATE'
+        if 'rotate_mobile_base' in traj.joint_names: return 'ROTATE'
+        return 'ARM_ONLY'
+
+if __name__ == '__main__':
+    MoveMe()
